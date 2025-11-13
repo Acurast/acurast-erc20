1fbb4a545d05ad30ce5efc7d07d6a99f1b3eee25
Mike Godenzi <m.godenzi@papers.ch>
2025-11-13 13:45:52 +0100

    feat/improved-restrictor

--- contracts/transfer-restrictor/TransferRestrictor.sol
+++ contracts/transfer-restrictor/TransferRestrictor.sol
@@ -1,18 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IERC20TransferRestrictor.sol";
 
-contract TransferRestrictor is IERC20TransferRestrictor {
+contract TransferRestrictor is IERC20TransferRestrictor, Ownable {
     mapping(address => bool) public fromDenyList;
+
+    event DenyFrom(address indexed from);
+    event AllowFrom(address indexed from);
     
-    constructor(address[] memory _fromDenyList) {
+    constructor(address[] memory _fromDenyList) Ownable(msg.sender) {
         for (uint i = 0; i < _fromDenyList.length; i++) {
             fromDenyList[_fromDenyList[i]] = true; 
         }
     }
 
+    function denyFrom(address from) external onlyOwner {
+        fromDenyList[from] = true;
+        emit DenyFrom(from);
+    }
+
+    function allowFrom(address from) external onlyOwner {
+        delete fromDenyList[from];
+        emit AllowFrom(from);
+    }
+
     function isTransferAllowed(address from, address to, uint256 value) external view returns (bool) {
-        return fromDenyList[from] == false;
+        return !fromDenyList[from];
     }
 }
